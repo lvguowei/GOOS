44073ae743d8873f48cea6b8d3f51a3228c49a7a
Guowei Lv <lvguowei1002@gmail.com>
2018-02-24 22:36:28 +0200

    the sniper has more to say the sniper acquires some state

--- src/test/java/com/guowei/lv/AuctionSniperTest.java
+++ src/test/java/com/guowei/lv/AuctionSniperTest.java
@@ -2,11 +2,13 @@ package com.guowei.lv;
 
 import org.jmock.Expectations;
 import org.jmock.Mockery;
+import org.jmock.States;
 import org.jmock.integration.junit4.JMock;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static com.guowei.lv.AuctionEventListener.PriceSource.FromOtherBidder;
+import static com.guowei.lv.AuctionEventListener.PriceSource.FromSniper;
 
 @RunWith(JMock.class)
 public class AuctionSniperTest {
@@ -14,13 +16,29 @@ public class AuctionSniperTest {
     private final SniperListener sniperListener = context.mock(SniperListener.class);
     private final Auction auction = context.mock(Auction.class);
     private final AuctionSniper sniper = new AuctionSniper(auction, sniperListener);
+    private final States sniperState = context.states("sniper");
 
     @Test
-    public void reportsLostWhenAuctionCloses() {
+    public void reportsLostWhenAuctionClosesImmediately() {
         context.checking(new Expectations() {{
             one(sniperListener).sniperLost();
         }});
 
+        sniper.auctionClosed();
+    }
+
+    @Test
+    public void reportsLostIfAuctionClosesWhenBidding() {
+        context.checking(new Expectations() {{
+            ignoring(auction);
+            allowing(sniperListener).sniperBidding();
+            then(sniperState.is("bidding"));
+
+            atLeast(1).of(sniperListener).sniperLost();
+            when(sniperState.is("bidding"));
+        }});
+
+        sniper.currentPrice(123, 45, FromOtherBidder);
         sniper.auctionClosed();
     }
 
@@ -34,5 +52,14 @@ public class AuctionSniperTest {
         }});
 
         sniper.currentPrice(price, increment, FromOtherBidder);
+    }
+
+    @Test
+    public void reportsIsWinningWhenCurrentPriceComesFromSniper() {
+        context.checking(new Expectations() {{
+            atLeast(1).of(sniperListener).sniperWinning();
+        }});
+
+        sniper.currentPrice(123, 45, FromSniper);
     }
 }
