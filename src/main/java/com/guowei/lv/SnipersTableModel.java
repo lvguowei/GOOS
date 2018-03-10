dec0872bb7c11bd58a1a3b707aa32d5c68b1b456
Guowei Lv <lvguowei1002@gmail.com>
2018-03-10 20:29:57 +0200

    object-oriented column

--- src/main/java/com/guowei/lv/SnipersTableModel.java
+++ src/main/java/com/guowei/lv/SnipersTableModel.java
@@ -11,11 +11,33 @@ public class SnipersTableModel extends AbstractTableModel {
     private SniperSnapshot snapshot = STARTING_UP;
 
     public enum Column {
-        ITEM_IDENTIFIER, LAST_PRICE, LAST_BID, SNIPER_STATE;
+        ITEM_IDENTIFIER {
+            @Override
+            public Object valueIn(SniperSnapshot snapshot) {
+                return snapshot.itemId;
+            }
+        }, LAST_PRICE {
+            @Override
+            public Object valueIn(SniperSnapshot snapshot) {
+                return snapshot.lastPrice;
+            }
+        }, LAST_BID {
+            @Override
+            public Object valueIn(SniperSnapshot snapshot) {
+                return snapshot.lastBid;
+            }
+        }, SNIPER_STATE {
+            @Override
+            public Object valueIn(SniperSnapshot snapshot) {
+                return textFor(snapshot.state);
+            }
+        };
 
         public static Column at(int offset) {
             return values()[offset];
         }
+
+        abstract public Object valueIn(SniperSnapshot snapshot);
     }
 
 
@@ -31,18 +53,7 @@ public class SnipersTableModel extends AbstractTableModel {
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-        switch (Column.at(columnIndex)) {
-            case ITEM_IDENTIFIER:
-                return snapshot.itemId;
-            case LAST_BID:
-                return snapshot.lastBid;
-            case LAST_PRICE:
-                return snapshot.lastPrice;
-            case SNIPER_STATE:
-                return textFor(snapshot.state);
-            default:
-                throw new IllegalArgumentException();
-        }
+        return Column.at(columnIndex).valueIn(snapshot);
     }
 
     public static String textFor(SniperState state) {
