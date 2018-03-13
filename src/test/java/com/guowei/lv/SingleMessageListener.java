cab5713345194d2d1f38774c99cc77c94f5d5c2b
Guowei Lv <lvguowei1002@gmail.com>
2018-03-13 20:57:44 +0200

    a diversion, fixing the failure message

--- src/test/java/com/guowei/lv/SingleMessageListener.java
+++ src/test/java/com/guowei/lv/SingleMessageListener.java
@@ -11,6 +11,7 @@ import java.util.concurrent.TimeUnit;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasProperty;
 
 public class SingleMessageListener implements MessageListener{
 
@@ -23,7 +24,6 @@ public class SingleMessageListener implements MessageListener{
 
     public void receivesAMessage(Matcher<? super String> messageMatcher) throws InterruptedException {
         final Message message = messages.poll(5, TimeUnit.SECONDS);
-        assertThat("Message", message, is(notNullValue()));
-        assertThat(message.getBody(), messageMatcher);
+        assertThat(message, hasProperty("body", messageMatcher));
     }
 }
